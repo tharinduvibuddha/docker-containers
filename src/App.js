2318af41f5f8e33b7682c8d04359068d84c1edd5
tharinduvibuddha <tharinduvibuddha@gmail.com>
2020-12-21 16:07:45 +0530

    text change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello There Tharindu.
+          Hello There Tharindu. I am changed.
         </p>
         <a
           className="App-link"
